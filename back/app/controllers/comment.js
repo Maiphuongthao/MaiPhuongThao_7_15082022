6945883e131dc189e81e5dbbabe12aadd7e36339
Thao <thao.maiphuong@outlook.fr>
2022-08-25 17:17:12 +0200

    post routes corrections

--- back/app/controllers/comment.js
+++ back/app/controllers/comment.js
@@ -7,9 +7,6 @@ const Comment = require("../models/comment");
 exports.readOneComment = (req, res, next) => {
     Comment.findById(req.params.id)
       .then((comment) => {
-        if (req.body.imageUrl) {
-          comment.imageUrl = `${req.protocol}://${req.get("host")}${comment.imageUrl}`;
-        }
         res.status(200).json(comment);
       })
       .catch((error) =>
@@ -19,3 +16,43 @@ exports.readOneComment = (req, res, next) => {
       );
   };
   
+
+  ///////////////READ ALL POSTS/////////////////////////
+
+exports.readAllComments = (req, res, next) => {
+    Comment.find({postId: req.body.postId})
+      .then((postWithComments) => {
+        res.status(200).json(postWithComments);
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: error,
+        });
+      });
+  };
+
+
+
+///////////CREATE COMMENT///////////////////
+
+exports.createComment = (req, res, next) => {
+    //parse objet to string
+    const commentObject = JSON.parse(req.body.comment);
+    //delete the id as it'll be generate automatique
+    delete commentObject._id;
+    //create new post
+    const post = new Comment({
+      ...commentObject,
+      userId: req.auth.userId,
+      imageUrl: req.file ? `/images/${req.file.filename}` : "",
+    });
+  
+    post
+      .save()
+      .then((newPost) => {
+        res.status(201).json(newPost);
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
+      });
+  };
